0a658630c4991b782f18e17d8dd2c288ef30d7be
Chris Howk <jchowk@gmail.com>
2018-09-27 11:16:28 -0400

    Cleanup

---
+++ build/lib/pyMCZ/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+p = os.path.abspath('./')
+if not os.path.exists(p + '/output'):
+    os.makedirs(p + '/output')
+
+__all__ = ["mcz", "metallicity", "metscales", "testcompleteness"]
--- build/scripts-3.6/mcz.py
+++ build/scripts-3.6/mcz.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/Users/howk/miniconda3/bin/python
 from __future__ import print_function
 import os
 import sys
